970e447443cf2d300340658fe76b9b78cfeb2f66
Mathis Renier <mathisrenier@Mathiss-MacBook-Pro.local>
2020-12-12 18:08:06 -0500

    finalized javabridge set up

--- preprocessor.py
+++ preprocessor.py
@@ -1,11 +1,34 @@
 # COMP 550 FINAL PROJECT
 
+import sys
 import weka.core.jvm as jvm
 import weka.core.packages as packages
 from weka.core.classes import complete_classname
 
-jvm.start(packages=True)
+def install_package(pkg):
+    # install package if necessary
+    if not packages.is_installed(pkg):
+        print("Installing %s..." % pkg)
+        packages.install_package(pkg)
+        print("Installed %s, please re-run script!" % pkg)
+        jvm.stop()
+        sys.exit(0)
+    print('Package already installed.')
 
-help(jvm.start)
 
-jvm.stop()
+def remove_package(pkg):
+    if packages.is_installed(pkg):
+        print("Removing %s..." % pkg)
+        packages.uninstall_package(pkg)
+        print("Removed %s, please re-run script!" % pkg)
+        jvm.stop()
+        sys.exit(0)
+    print('No such package is installed')
+
+if __name__ == '__main__':
+    jvm.start(packages=True)
+
+    install_package('AffectiveTweets')
+
+
+    jvm.stop()
