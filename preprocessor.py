eefa58d3c7e00aabd3ec5d12ad91dff5b83c041a
Mathis Renier <reniermathis@gmail.com>
2020-12-15 15:59:19 -0500

    Added get_affect_vectors()

--- preprocessor.py
+++ preprocessor.py
@@ -97,6 +97,67 @@ def get_ordered_lists(instances):
     return tweets, sentiments, intensities
 
 
+def get_affect_vectors(type='train', emotion='anger'):
+    assert type == 'train' or type == 'test', 'Invalid instance type, try train/test'
+    assert emotion == 'anger' or emotion == 'fear' or emotion == 'joy' or emotion == 'sad', 'Invalid emotion, try anger/fear/joy/sad'
+
+    # load separated instances
+    anger_train, fear_train, joy_train, sad_train = loader.load_instances_separated(type='train')
+    anger_test, fear_test, joy_test, sad_test = loader.load_instances_separated(type='test')
+
+    # preprocess separated instances
+    for i in [anger_train, fear_train, joy_train, sad_train, anger_test, fear_test, joy_test, sad_test]:
+        i = preprocessor(i, stopwords=True)
+
+    # create ordered lists from separated instances
+    tweets_anger_train, sentiments_anger_train, intensities_anger_train = get_ordered_lists(anger_train)
+    tweets_fear_train, sentiments_fear_train, intensities_fear_train = get_ordered_lists(fear_train)
+    tweets_joy_train, sentiments_joy_train, intensities_joy_train = get_ordered_lists(joy_train)
+    tweets_sad_train, sentiments_sad_train, intensities_sad_train = get_ordered_lists(sad_train)
+
+    tweets_anger_test, sentiments_anger_test, intensities_anger_test = get_ordered_lists(anger_test)
+    tweets_fear_test, sentiments_fear_test, intensities_fear_test = get_ordered_lists(fear_test)
+    tweets_joy_test, sentiments_joy_test, intensities_joy_test = get_ordered_lists(joy_test)
+    tweets_sad_test, sentiments_sad_test, intensities_sad_test = get_ordered_lists(sad_test)
+
+    # create csv vector files for separated tweets
+    # affective_vectorizer(tweets_anger_train, 'train/anger.csv')
+    # affective_vectorizer(tweets_fear_train, 'train/fear.csv')
+    # affective_vectorizer(tweets_joy_train, 'train/joy.csv')
+    # affective_vectorizer(tweets_sad_train, 'train/sad.csv')
+    #
+    # affective_vectorizer(tweets_anger_test, 'test/anger.csv')
+    # affective_vectorizer(tweets_fear_test, 'test/fear.csv')
+    # affective_vectorizer(tweets_joy_test, 'test/joy.csv')
+    # affective_vectorizer(tweets_sad_test, 'test/sad.csv')
+
+    # extract vectors from csv for separated datasets
+    anger_vect_train = extract_vectors('data/affect-vectors/train/anger.csv')
+    fear_vect_train = extract_vectors('data/affect-vectors/train/fear.csv')
+    joy_vect_train = extract_vectors('data/affect-vectors/train/joy.csv')
+    sad_vect_train = extract_vectors('data/affect-vectors/train/sad.csv')
+
+    anger_vect_test = extract_vectors('data/affect-vectors/test/anger.csv')
+    fear_vect_test = extract_vectors('data/affect-vectors/test/fear.csv')
+    joy_vect_test = extract_vectors('data/affect-vectors/test/joy.csv')
+    sad_vect_test = extract_vectors('data/affect-vectors/test/sad.csv')
+
+    return {
+        'train': {
+            'anger': (anger_vect_train, intensities_anger_train),
+            'fear': (fear_vect_train, intensities_fear_train),
+            'joy': (joy_vect_train, intensities_joy_train),
+            'sad': (sad_vect_train, intensities_sad_train),
+        },
+        'test': {
+            'anger': (anger_vect_test, intensities_anger_test),
+            'fear': (fear_vect_test, intensities_fear_test),
+            'joy': (joy_vect_test, intensities_joy_test),
+            'sad': (sad_vect_test, intensities_sad_test),
+        },
+    }[type][emotion]
+
+
 if __name__ == '__main__':
     # load whole instances
     train_instances, test_instances = loader.load_instances()
@@ -157,5 +218,3 @@ if __name__ == '__main__':
     fear_vect_test = extract_vectors('data/affect-vectors/test/fear.csv')
     joy_vect_test = extract_vectors('data/affect-vectors/test/joy.csv')
     sad_vect_test = extract_vectors('data/affect-vectors/test/sad.csv')
-
-    print(tweets_sad_test)
